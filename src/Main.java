6f3d04a790ad7591f65a80348989848f22c0969b
gablon29 <gabriellondero40@gmail.com>
2023-10-25 18:05:19 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static String mostrarFrase(int num) {
+        return ("mi primer metodo que retorna el numero que viene por parametro " + num);
+    }
+    public static void main(String[] args) {
+        int numero = 1;
+        System.out.println(mostrarFrase(numero));
+    }
+}
